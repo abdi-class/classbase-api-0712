723aecd38c3023e627d315965154e5513da51595
nicholas-0101 <nsvertian@gmail.com>
2025-08-22 15:35:19 +0700

    feat: create feature crud blog (#1)

--- src/controllers/blog.controller.ts
+++ src/controllers/blog.controller.ts
@@ -1,9 +1,26 @@
 import { NextFunction, Request, Response } from "express";
+import { prisma } from "../config/prisma";
 
 class BlogController {
   public async createBlog(req: Request, res: Response, next: NextFunction) {
     try {
-      //
+      console.log(req.body);
+      const { title, thumbnail, categoryId, content } = req.body;
+      const newBlog = await prisma.blogs.create({
+        data: {
+          title,
+          thumbnail,
+          categoryId,
+          content,
+          accountId:parseInt(res.locals.decript.id)
+        },
+      });
+
+      res.status(200).send({
+        success: true,
+        message: "Blog added successfully",
+        newBlog,
+      });
     } catch (error) {
       next(error);
     }
